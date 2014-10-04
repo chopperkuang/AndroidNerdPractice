ac79d5653b317f699d54727fc2aea681b958ca1e
kuang <chopperkuang@gmail.com>
2014-10-04 22:44:37 +0800

    完成单页面的GeoQuiz的版本

--- GeoQuiz/src/net/kkuang/example/GeoQuiz/QuizActivity.java
+++ GeoQuiz/src/net/kkuang/example/GeoQuiz/QuizActivity.java
@@ -5,24 +5,48 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class QuizActivity extends Activity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mBackButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    private List<TrueFalse> mQuestionBank = new ArrayList<TrueFalse>(){{
+        add(new TrueFalse(R.string.question_oceans, true));
+        add(new TrueFalse(R.string.question_mideast, false));
+        add(new TrueFalse(R.string.question_africa, false));
+        add(new TrueFalse(R.string.question_americas, true));
+        add(new TrueFalse(R.string.question_china, true));
+    }};
+
+    private int mCurrentIndex = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mTrueButton = (Button) findViewById(R.id.true_button);
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toNextQuestion();
+            }
+        });
 
+        mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_LONG).show();
+                checkAnswer(true);
             }
         });
 
@@ -30,15 +54,62 @@ public class QuizActivity extends Activity {
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
             }
         });
+
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toNextQuestion();
+            }
+        });
+
+        mBackButton = (Button) findViewById(R.id.back_button);
+        mBackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toBackQuestion();
+            }
+        });
+
+        updateQuestion();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_quiz, menu);
         return true;
+    }
+
+    private void updateQuestion() {
+        mQuestionTextView.setText(mQuestionBank.get(mCurrentIndex).getQuestion());
+    }
+
+    private void toNextQuestion() {
+        mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.size();
+        updateQuestion();
+    }
+
+    private void toBackQuestion() {
+        if(mCurrentIndex == 0) {
+            Toast.makeText(this, R.string.back_over, Toast.LENGTH_SHORT).show();
+        }
+        mCurrentIndex = mCurrentIndex <= 0 ? 0 :  (mCurrentIndex - 1) % mQuestionBank.size();
+        updateQuestion();
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {
+        boolean answerIsTrue = mQuestionBank.get(mCurrentIndex).isTrueQuestion();
+
+        int messageResId = 0;
+        if(userPressedTrue == answerIsTrue) {
+            messageResId = R.string.correct_toast;
+        } else {
+            messageResId = R.string.incorrect_toast;
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 
 }
